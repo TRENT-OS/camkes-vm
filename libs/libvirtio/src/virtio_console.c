7330cbb5e2ca0e1635a48df704c92dd3e0aaec52
Axel Heider <axel.heider@hensoldt.net>
2023-05-30 03:47:50 +0200

    libvirtio: improve failure handling
    Signed-off-by: Axel Heider <axel.heider@hensoldt.net>

--- libs/libvirtio/src/virtio_console.c
+++ libs/libvirtio/src/virtio_console.c
@@ -63,11 +63,18 @@ virtio_con_t *virtio_console_init(vm_t *vm, console_putchar_fn_t putchar,
     ioport_range_t virtio_port_range = {0, 0, VIRTIO_IOPORT_SIZE};
     virtio_con = common_make_virtio_con(vm, pci, io_ports, virtio_port_range, IOPORT_FREE,
                                         VIRTIO_INTERRUPT_PIN, VIRTIO_CON_PLAT_INTERRUPT_LINE, backend);
+    if (!virtio_con) {
+        ZF_LOGE("Failed to initialise virtio con driver");
+        free(virtio_con);
+        return NULL;
+    }
+
     console_cookie->virtio_con = virtio_con;
     console_cookie->vm = vm;
     err = vm_register_irq(vm->vcpus[BOOT_VCPU], VIRTIO_CON_PLAT_INTERRUPT_LINE, &virtio_console_ack, NULL);
     if (err) {
         ZF_LOGE("Failed to register console irq");
+        /* ToDo: free virtio_con? */
         free(console_cookie);
         return NULL;
     }
--- libs/libvirtio/src/virtio_net.c
+++ libs/libvirtio/src/virtio_net.c
@@ -135,6 +135,13 @@ virtio_net_t *virtio_net_init(vm_t *vm, virtio_net_callbacks_t *callbacks,
     driver_cookie->virtio_net = virtio_net;
     driver_cookie->vm = vm;
     int err = vm_register_irq(vm->vcpus[BOOT_VCPU], VIRTIO_NET_PLAT_INTERRUPT_LINE, &virtio_net_ack, NULL);
+    if (err) {
+        ZF_LOGE("Failed to register net irq");
+        /* ToDo: free virtio_net? */
+        free(driver_cookie);
+        return NULL;
+    }
+
     if (callbacks) {
         driver_cookie->callbacks.tx_callback = callbacks->tx_callback;
         driver_cookie->callbacks.irq_callback = callbacks->irq_callback;
